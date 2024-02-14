6fe5c00ec971b3ac4cf9b7d3a4c4994ac944ac56
Sungmok Will Cho <131221622+sungmogi@users.noreply.github.com>
2024-02-14 11:15:55 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import './App.css';
-import io from 'socket.io-client';
-import { useState, useEffect } from 'react';
 import Home from './Home';
 import Chat from './Chat'
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
-const socket = io.connect('http://localhost:8000');
 function App() {
   return (
     <BrowserRouter>
